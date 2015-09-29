886c783a4cfd908d3b4fe2bd770c3d50130a45d2
Mathijs Mortimer <mathijs@mortimer.nl>
2015-09-29 10:46:34 +0200

    Added basic ServiceMonitor class showing current users and network connections usin psutil

--- controller/service_monitor.py
+++ controller/service_monitor.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
 
-# Configuration Manager Module
+# Service Monitor
+#
+# Module to monitor all application components. This
+# relies heavily on the psutil package which gives a cross
+# platform interface to various OS resources
+
+import psutil
+
+
+class ServiceMonitor():
+    """ Monitoring the various application daemons and
+    OS statistics """
+
+    def net_connections(self):
+        """ Returns the current network connections """
+        return psutil.net_connections()
+
+    def users(self):
+        """ Return current logged on users """
+        return psutil.users()
 
 
 def main():
-    pass
+    service_monitor = ServiceMonitor()
+    print("Current network conn {}".format(service_monitor.net_connections()))
+    print("Current logged on users {}".format(service_monitor.users()))
 
 if __name__ == '__main__':
     main()
