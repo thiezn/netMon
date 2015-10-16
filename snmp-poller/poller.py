33d1aa4510246a0c2ed8db15785dd2c748367ded
Mathijs Mortimer <mathijs@mortimer.nl>
2015-10-16 14:08:31 +0200

    Starting with writing asyncio snmp poller
    At the moment just using a pysnmp example for a simple get request

---
+++ snmp-poller/poller.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import asyncio
+from pysnmp.hlapi.asyncio import (SnmpEngine, CommunityData, ContextData,
+                                  ObjectType, ObjectIdentity, getCmd,
+                                  UdpTransportTarget)
+
+
+@asyncio.coroutine
+def run():
+    snmpEngine = SnmpEngine()
+    (errorIndication,
+     errorStatus,
+     errorIndex,
+     varBinds) = yield from getCmd(snmpEngine,
+                                   CommunityData('public', mpModel=0),
+                                   UdpTransportTarget(('demo.snmplabs.com', 161)),
+                                   ContextData(),
+                                   ObjectType(ObjectIdentity('SNMPv2-MIB',
+                                                             'sysDescr', 0)))
+
+    if errorIndication:
+        print(errorIndication)
+    elif errorStatus:
+        print('%s at %s' % (errrorStatus.prettyPrint(),
+                            errorIndex and
+                            varBinds[int(errorIndex)-1][0] or '?'))
+    else:
+        for varBind in varBinds:
+            print(' = '.join([x.prettyPrint() for x in varBind]))
+
+    snmpEngine.transportDispatcher.closeDispatcher()
+
+asyncio.get_event_loop().run_until_complete(run())
